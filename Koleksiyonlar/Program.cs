35f70e254eb31831c4a4b9037719e0b66fff7cb5
tunahankarakokk <tunahankarakok0@gmail.com>
2022-08-25 03:14:25 +0300

    25.01.2022 ders sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // steak             //heap     //değerler
+            //string[] isimler = new string[] {"Engin","Murat","Kerem","Halil"};
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler = new string[5];
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            //toplu comment alma ctrl+k-ctrl+c geri alma ctrl+k-ctrl+u
+            List<string> isimler2 = new List<string> {"Engin","Murat","Kerem","Halil"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+            
+        
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class ProductManager
+    {
+        /*
+         git ekle git sil git güncelle söyledikten sonra emiri bitirdikten sonra işlem sonucunda ekstra bilgiye ihtiyaç yoksa 
+        void tanımlanır git yap bitir
+         */
+
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+" eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+      
+    }
+}
